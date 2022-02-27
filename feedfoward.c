f7683ac50a4f9eacab14c731131b82938d0dae91
iomarino <50249831+iomarino@users.noreply.github.com>
2022-02-27 16:46:00 +0300

    Update feedfoward.c

--- feedfoward.c
+++ feedfoward.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <time.h>
 
-void read_csv(int row, int col, char *filename, double **data){
+void read_csv(int row, int col, char *filename, long double **data){
 	FILE *file;
 	file = fopen(filename, "r");
 
@@ -12,7 +12,7 @@ void read_csv(int row, int col, char *filename, double **data){
     char line[4098];
 	while (fgets(line, 4098, file) && (i < row))
     {
-    	// double row[ssParams->nreal + 1];
+    	// long double row[ssParams->nreal + 1];
         char* tmp = strdup(line);
 
 	    int j = 0;
@@ -26,115 +26,105 @@ void read_csv(int row, int col, char *filename, double **data){
     }
 }
 void sample(size_t* v, size_t n, size_t r) {
-  if (n>1) {
-    for(int i=0; i<r; i++) {
-      size_t a = (int)((double)rand()/RAND_MAX*n);
-      v[i] = a;
-    }
+	for(int i=0; i<r; i++) {
+    size_t a = (int)((long double)rand()/RAND_MAX*n);
+    v[i] = a;
   }
 }
-void shuffle(size_t* v, size_t n) {
-    if (n>1) {
-        size_t i;
-        for(i=0;i<n-1; i++) {
-          size_t j = i+rand()/(RAND_MAX/(n-i)+1);
-          int t = v[j];
-          v[j] = v[i];
-          v[i] = t;
-        }
-    }
-}
-double sig(double z) {
+long double sig(long double z) {
 	return 1/(1+exp(-z));
 }
-double dsig(double z) {
+long double dsig(long double z) {
 	return sig(z)*(1-sig(z));
 }
-double relu(double z) {
+long double relu(long double z) {
   if(z>0)
     return z;
   else
     return 0;
 }
-double drelu(double z) {
+long double drelu(long double z) {
   if(z>0)
     return 1;
   else
     return 0;
 }
-double datan(double z) {
+long double datan(long double z) {
   return 1/(1+z*z);
 }
-double dtanh(double z) {
-  return 1/(cosh(z)*cosh(z));
+long double dtanh(long double z) {
+  return 4/pow(exp(-z)+exp(z),2);
 }
-double g(double z) { return atan(z); }
-double dg(double z) { return datan(z); }
+// hidden layers activation function
+long double g(long double z) { return relu(z); }
+long double dg(long double z) { return drelu(z); }
+// output layers activation function (not used; softmax implemented in code directly)
+long double h(double z) { return sig(z); }
+long double dh(double z) { return dsig(z); }
 
 int main() {
   int i, j, k, l, e;
-  const int L = 4; // # of layers
-  const int K[L] = {784, 256, 128, 10}; // # of neurons per layer
-  double ** z = (double **)malloc(L*sizeof(double**));
+  const int L = 8; // # of layers
+  const int K[L] = {784, 512, 256, 128, 64, 32, 16, 10}; // # of neurons per layer
+  long double ** z = (long double **)malloc(L*sizeof(long double**));
   for (l=1; l<L; l++) {
-    z[l] = (double *) malloc(K[l]*sizeof(double));
+    z[l] = (long double *) malloc(K[l]*sizeof(long double));
   }
-  double ** b = (double **)malloc(L*sizeof(double**));
+  long double ** b = (long double **)malloc(L*sizeof(long double**));
   for (l=1; l<L; l++) {
-    b[l] = (double *) malloc(K[l]*sizeof(double));
+    b[l] = (long double *) malloc(K[l]*sizeof(long double));
   }
-  double *** w = (double ***)malloc(L*sizeof(double**));
+  long double *** w = (long double ***)malloc(L*sizeof(long double**));
   for (l=1; l<L; l++) {
-    w[l] = (double **) malloc(K[l]*sizeof(double *));
+    w[l] = (long double **) malloc(K[l]*sizeof(long double *));
     for (k=0; k<K[l]; k++) {
-      w[l][k] = (double *)malloc(K[l-1]*sizeof(double));
+      w[l][k] = (long double *)malloc(K[l-1]*sizeof(long double));
     }
   }
-  double ** a = (double **)malloc(L*sizeof(double**));
+  long double ** a = (long double **)malloc(L*sizeof(long double**));
   for (l=0; l<L; l++) {
-    a[l] = (double *) malloc(K[l]*sizeof(double));
+    a[l] = (long double *) malloc(K[l]*sizeof(long double));
   }
-  double ** dCa = (double **)malloc(L*sizeof(double**));
+  long double ** dCa = (long double **)malloc(L*sizeof(long double**));
   for (l=1; l<L; l++) {
-    dCa[l] = (double *) malloc(K[l]*sizeof(double));
+    dCa[l] = (long double *) malloc(K[l]*sizeof(long double));
   }
-  double ** dCb = (double **)malloc(L*sizeof(double**));
+  long double ** dCb = (long double **)malloc(L*sizeof(long double**));
   for (l=1; l<L; l++) {
-    dCb[l] = (double *) malloc(K[l]*sizeof(double));
+    dCb[l] = (long double *) malloc(K[l]*sizeof(long double));
   }
-  double *** dCw = (double ***)malloc(L*sizeof(double**));
+  long double *** dCw = (long double ***)malloc(L*sizeof(long double**));
   for (l=1; l<L; l++) {
-    dCw[l] = (double **) malloc(K[l]*sizeof(double *));
+    dCw[l] = (long double **) malloc(K[l]*sizeof(long double *));
     for (k=0; k<K[l]; k++) {
-      dCw[l][k] = (double *)malloc(K[l-1]*sizeof(double));
+      dCw[l][k] = (long double *)malloc(K[l-1]*sizeof(long double));
     }
   }
-  const double lr = 0.05;
   // generate random weights and biases
   srand(time(0));
   for(l=1; l<L; l++) {
     for(k=0; k<K[l]; k++) {
-      b[l][k] = 4*(double)rand()/RAND_MAX-2;
+      b[l][k] = 2*(long double)rand()/RAND_MAX-1;
       for(j=0; j<K[l-1]; j++) {
-        w[l][k][j] = 2*(double)rand()/RAND_MAX-1;
+        w[l][k][j] = (2*(long double)rand()/RAND_MAX-1)/sqrt(K[l-1]);
       }
     }
   }
   // source train inputs and outputs
   unsigned int N = 60000; // dimension of train set
-  double ** x = (double **)malloc(N*sizeof(double**));
+  long double ** x = (long double **)malloc(N*sizeof(long double**));
   for (i=0; i<N; i++) {
-    x[i] = (double *)malloc(K[0]*sizeof(double));
+    x[i] = (long double *)malloc(K[0]*sizeof(long double));
   }
-  double ** y = (double **)malloc(N*sizeof(double**));
+  long double ** y = (long double **)malloc(N*sizeof(long double**));
   for (i=0; i<N; i++) {
-    y[i] = (double *)malloc(K[L-1]*sizeof(double));
+    y[i] = (long double *)malloc(K[L-1]*sizeof(long double));
   }
 	int row = N;
 	int col = K[0]+1;
-	double ** data = (double **)malloc(row*sizeof(double *));
-	for (int i = 0; i < row; ++i){
-		data[i] = (double *)malloc(col*sizeof(double));
+	long double ** data = (long double **)malloc(row*sizeof(long double *));
+	for (i=0; i<row; i++){
+		data[i] = (long double *)malloc(col*sizeof(long double));
 	}
 	read_csv(row, col, "mnist_train.csv", data);
   for(i=0; i<N; i++) {
@@ -147,12 +137,13 @@ int main() {
       x[i][j] = data[i][j+1];
     }
   }
-
-  const unsigned int epochs = 60000;
-  const unsigned int sgd_size = 100;
+	const long double rho = 0.025;
+	const long double lambda = 8;
+  const unsigned int sgd_size = 30;
+	const unsigned int steps = 20000;
   size_t index[sgd_size];
   // train
-  for(e=0; e<epochs; e++) {
+  for(e=0; e<steps; e++) {
 		// random sample for SGD
     sample(index, N, sgd_size);
     // reset gradient
@@ -180,20 +171,24 @@ int main() {
           a[l][k] = g(z[l][k]);
         }
       }
-      // exception for output layer
+      // softmax for output layer
+			long double expsum = 0;
       for(k=0; k<K[L-1]; k++) {
         z[L-1][k] = b[L-1][k];
         for(j=0; j<K[L-2]; j++) {
           z[L-1][k] += w[L-1][k][j]*a[L-2][j];
         }
-        a[L-1][k] = sig(z[L-1][k]);
+				expsum += exp(z[L-1][k]);
       }
-      // calculate derivatives
+			for(k=0; k<K[L-1]; k++) {
+				a[L-1][k] = exp(z[L-1][k])/expsum;
+			}
+      // calculate partial derivatives
       for(k=0; k<K[L-1]; k++) {
         dCa[L-1][k] = a[L-1][k]-y[index[i]][k];
-        dCb[L-1][k] += dsig(z[L-1][k])*dCa[L-1][k];
+        dCb[L-1][k] += dCa[L-1][k];
         for(j=0; j<K[L-2]; j++) {
-          dCw[L-1][k][j] += a[L-2][j]*dsig(z[L-1][k])*dCa[L-1][k];
+          dCw[L-1][k][j] += a[L-2][j]*dCa[L-1][k]-(lambda/N)*w[L-1][k][j];
         }
       }
       for(l=L-2; l>0; l--) {
@@ -204,7 +199,7 @@ int main() {
           }
           dCb[l][k] += dg(z[l][k])*dCa[l][k];
           for(j=0; j<K[l-1]; j++) {
-            dCw[l][k][j] += a[l-1][j]*dg(z[l][k])*dCa[l][k];
+            dCw[l][k][j] += a[l-1][j]*dg(z[l][k])*dCa[l][k]-(lambda/N)*w[l][k][j];
           }
         }
       }
@@ -212,9 +207,9 @@ int main() {
     // gradient descent
     for(l=1; l<L; l++) {
       for(k=0; k<K[l]; k++) {
-        b[l][k] += -lr*dCb[l][k]/sgd_size;
+        b[l][k] += -rho*dCb[l][k]/sgd_size;
         for(j=0; j<K[l-1]; j++) {
-          w[l][k][j] += -lr*dCw[l][k][j]/sgd_size;
+          w[l][k][j] += -rho*dCw[l][k][j]/sgd_size;
         }
       }
     }
@@ -223,16 +218,16 @@ int main() {
   // testing
   N = 10000; // dimension of test set
   free(y);
-  realloc(x, N*sizeof(double**));
+  x = realloc(x, N*sizeof(long double**));
   for (i=0; i<N; i++) {
-    x[i] = (double *)malloc(K[0]*sizeof(double));
+    x[i] = (long double *)malloc(K[0]*sizeof(long double));
   }
   // source test inputs and outputs
 	row = N;
 	col = K[0]+1;
-  realloc(data, row*sizeof(double *));
+  data = realloc(data, row*sizeof(long double *));
 	for (i=0; i<row; i++){
-		data[i] = (double *)malloc(col*sizeof(double));
+		data[i] = (long double *)malloc(col*sizeof(long double));
 	}
 	read_csv(row, col, "mnist_test.csv", data);
   for(i=0; i<N; i++) {
@@ -255,24 +250,29 @@ int main() {
         a[l][k] = g(z[l][k]);
       }
     }
-    for(k=0; k<K[L-1]; k++) {
-      z[L-1][k] = b[L-1][k];
-      for(j=0; j<K[L-2]; j++) {
-        z[L-1][k] += w[L-1][k][j]*a[L-2][j];
-      }
-      a[L-1][k] = sig(z[L-1][k]);
-      fprintf(fp, "%lf,", a[L-1][k]);
-    }
+		// exception for output layer
+		long double expsum = 0;
+		for(k=0; k<K[L-1]; k++) {
+			z[L-1][k] = b[L-1][k];
+			for(j=0; j<K[L-2]; j++) {
+				z[L-1][k] += w[L-1][k][j]*a[L-2][j];
+			}
+			expsum += exp(z[L-1][k]);
+		}
+		for(k=0; k<K[L-1]; k++) {
+			a[L-1][k] = exp(z[L-1][k])/expsum;
+			fprintf(fp, "%Lf,", a[L-1][k]);
+		}
     fprintf(fp, "\n");
   }
   fclose(fp);
   // output parameters to file
-  fp = fopen("parameters.csv", "w");
+  fp = fopen("parameters", "w");
   for(l=1; l<L; l++) {
     for(k=0; k<K[l]; k++) {
-      fprintf(fp, "%lf\n", b[k][j]);
+      fprintf(fp, "%Lf\n", b[l][k]);
       for(j=0; j<K[l-1]; j++) {
-        fprintf(fp, "%lf ", w[l][k][j]);
+        fprintf(fp, "%Lf ", w[l][k][j]);
       }
       fprintf(fp, "\n");
     }
